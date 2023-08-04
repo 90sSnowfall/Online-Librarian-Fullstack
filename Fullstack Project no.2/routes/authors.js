94f561009b51fad7ef3eb1ce0fc1e6567e957b22
90sSnowfall <nicolaeciprian.dragan0424@gmail.com>
2023-08-04 15:39:17 +0300

    Broken update

--- Fullstack Project no.2/routes/authors.js
+++ Fullstack Project no.2/routes/authors.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router()
 const Author = require('../models/author');
 
+
 //All Authors route
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if(req.query.name != null && req.query.name != '') {
+        searchOptions.name = RegExp(req.query.name, 'i');
+    }
+    try{
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        });
+    } catch {
+        res.redirect('/');
+    }
     res.render('authors/index');
 });
 
@@ -11,23 +25,37 @@ router.get('/new', (req, res)=> {
     res.render('authors/new', { author: new Author()});
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     });
 
-    author.save().
-    then((newAuthor) => {
+    try{
+        const newAuthor = await author.save();
         // res.redirect(`authors/${author.id}`);
         res.render('authors');
         console.log('redirected to authors');
-    }).
-    catch((err) => {
-        res.render('authors/new',{
+
+    }catch(error){
+        res.render('authors/new', {
             author: author,
-            errMessage:'Error Creating Author'
+            errMessage: 'Error creating Author.'
         });
-    });
+    }
+
+    //It works but kinda old, and clunky
+    // author.save().
+    // then((newAuthor) => {
+    //     // res.redirect(`authors/${author.id}`);
+    //     res.render('authors');
+    //     console.log('redirected to authors');
+    // }).
+    // catch((err) => {
+    //     res.render('authors/new',{
+    //         author: author,
+    //         errMessage:'Error Creating Author'
+    //     });
+    // });
 
         //DEPRECATED \/
     // author.save((err, newAuthor) => {
